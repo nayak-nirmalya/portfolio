a29ea3279be961c0cbe58d7b7ecb2cb87299eeb4
Nirmalya Nayak <n.nayak263@gmail.com>
2022-06-29 12:32:08 +0530

    changed how title gets rendered and updated title icon.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ import Social from './components/Social'
 import Footer from './components/Footer'
 import Skills from './components/Slills'
 import Education from './components/Education'
-import { useEffect, useState } from 'react'
 
 function App() {
-  // eslint-disable-next-line
-  const [title, setTitle] = useState('Nirmalya Nayak | Portfolio')
-
-  useEffect(() => {
-    document.title = title
-  }, [title])
-
   return (
     <div className="App">
       <Header />
