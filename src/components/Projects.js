7afde2f12e606eaad1fe3f871bd610a613491c74
Nirmalya Nayak <n.nayak263@gmail.com>
2022-06-29 12:08:00 +0530

    added raffle/lottery dapp to the project list.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import eth_plus from '../assets/eth_plus.png'
 import todo_dapp from '../assets/todo_dapp.png'
 import faucet from '../assets/faucet.png'
 import eth_crowd from '../assets/eth_crowd.png'
+import raffle_pic from '../assets/raffle-hardhat-next.png'
 
 const Projects = () => {
   return (
@@ -10,6 +11,7 @@ const Projects = () => {
       <h2>My Projects</h2>
 
       <div className="projects__cards">
+        {/* ETH Plus Exchange */}
         <div className="projects__card">
           <h3>ETH Plus Exchange</h3>
           <img src={eth_plus} alt="ETH Plus Exchange" />
@@ -31,6 +33,7 @@ const Projects = () => {
           </div>
         </div>
 
+        {/* CrowdFunding DApp */}
         <div className="projects__card">
           <h3>CrowdFunding DApp</h3>
           <img src={eth_crowd} alt="ETH Kickstarter" />
@@ -51,6 +54,28 @@ const Projects = () => {
           </div>
         </div>
 
+        {/* Raffle/Lottery DApp */}
+        <div className="projects__card">
+          <h3>Raffle/Lottery DApp</h3>
+          <img src={raffle_pic} alt="Raffle/Lottery DApp" />
+          <p>
+            It's a DApp where multiple user can enter a raffle with pre defined
+            Entrance Fee. After a time interval a random user is pciked as
+            winner and gets all Ethers of the contract.
+          </p>
+          <div className="centered">
+            <a
+              href="https://github.com/nayak-nirmalya/raffle-dapp"
+              target="_blank"
+              rel="noreferrer"
+              className="button"
+            >
+              Code
+            </a>
+          </div>
+        </div>
+
+        {/* ETH ToDo List DApp */}
         <div className="projects__card">
           <h3>ETH ToDo List DApp</h3>
           <img src={todo_dapp} alt="ETH ToDo List DApp" />
@@ -71,6 +96,7 @@ const Projects = () => {
           </div>
         </div>
 
+        {/* ETH Faucet */}
         <div className="projects__card">
           <h3>ETH Faucet</h3>
           <img src={faucet} alt="ETH Faucet" />
