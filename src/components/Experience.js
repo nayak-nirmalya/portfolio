8ffe37e52ea7fe462dbe71aac69aa421e901ccfa
Nirmalya Nayak <n.nayak263@gmail.com>
2022-11-25 19:38:09 +0530

    updated experience details

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,7 +4,7 @@ const Experience = () => {
       <h2>Experience</h2>
 
       <ul>
-        <li>
+        {/* <li>
           <i>Cognizant</i> - <strong>SDE Intern</strong>, 2021 (March - June)
           <ul>
             <li>Studied Java, Spring Framework</li>
@@ -15,6 +15,13 @@ const Experience = () => {
           July - 2022 March
           <ul>
             <li>Wrote selenium automation scripts on client's platform</li>
+          </ul>
+        </li> */}
+        <li>
+          <i>Cognizant</i> - <strong>Programmer Analyst</strong>
+          <ul>
+            <li>Worked in Java, Spring Framework</li>
+            <li>Automated test cases with Selenium</li>
           </ul>
         </li>
       </ul>
