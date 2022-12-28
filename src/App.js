bc2b291b9ea312317f1dd6c0cc9b007c142ba0ae
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-28 15:42:45 +0530

    added dark mode support

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { createContext, useState } from 'react'
+import ReactSwitch from 'react-switch'
+
 // Import Components
 import Header from './components/Header'
 import About from './components/About'
@@ -8,18 +11,37 @@ import Footer from './components/Footer'
 import Skills from './components/Slills'
 import Education from './components/Education'
 
+export const ThemeContext = createContext(null)
+
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'))
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <About />
-      <Projects />
-      <Experience />
-      <Skills />
-      <Education />
-      <Social />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div className="switch">
+          {/* <label>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label> */}
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={theme === 'dark'}
+            height={20}
+            width={50}
+          />
+        </div>
+        <Header />
+        <About />
+        <Projects />
+        <Experience />
+        <Skills />
+        <Education />
+        <Social />
+        <Footer />
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
