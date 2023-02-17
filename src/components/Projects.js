80ca18d0ac83f96efec3d45e1f9409d25b3423e9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-17 10:28:07 +0530

    added @emotion/react

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,6 @@
+import Slider from "react-slick";
+import SliderWrapper from "../_SlickSliderStyle";
+
 import ETHPlus from "../components/projects/ETHPlus";
 import ToDoDApp from "../components/projects/ToDoDApp";
 import ETHDaddy from "../components/projects/ETHDaddy";
@@ -13,11 +16,48 @@ import ETHMarketplace from "../components/projects/ETHMarketplace";
 import CrowdFundingDApp from "../components/projects/CrowdFundingDApp";
 
 const Projects = () => {
+  const settings = {
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    speed: 500,
+    arrows: false,
+    adaptiveHeight: true,
+    appendDots: (dots) => <ul>{dots}</ul>,
+    customPaging: (i) => (
+      <div className="ft-slick__dots--custom">
+        <div className="loading" />
+      </div>
+    ),
+  };
+
   return (
     <section className="projects">
       <h2>My Projects</h2>
 
-      <div className="projects__cards">
+      <SliderWrapper>
+        <Slider settings={settings}>
+          <RedditFirebase />
+          <ETHPlus />
+          <NodeShop />
+          <MillowDApp />
+          <GraphQLBlog />
+          <ETHDaddy />
+          <ToDoDeno />
+          <CrowdFundingDApp />
+          <NFTMarketplace />
+          <ETHMarketplace />
+          <RaffleDApp />
+          <ToDoDApp />
+          <ETHFaucet />
+        </Slider>
+      </SliderWrapper>
+
+      {/* <div className="projects__cards">
         <RedditFirebase />
         <ETHPlus />
         <NodeShop />
@@ -31,7 +71,7 @@ const Projects = () => {
         <RaffleDApp />
         <ToDoDApp />
         <ETHFaucet />
-      </div>
+      </div> */}
     </section>
   );
 };
