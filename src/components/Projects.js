75e22305eefdfb1ad8d0aec584cd9afeac3aee3c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-22 09:43:48 +0530

    changed stylings

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,14 +26,14 @@ const Projects = () => {
       breakpoint: { max: 3000, min: 1024 },
       items: 2.8,
     },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
+    // tablet: {
+    //   breakpoint: { max: 1024, min: 464 },
+    //   items: 2,
+    // },
+    // mobile: {
+    //   breakpoint: { max: 464, min: 0 },
+    //   items: 1,
+    // },
   };
 
   // https://github.com/YIZHUANG/react-multi-carousel/issues/14
