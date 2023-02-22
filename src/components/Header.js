819b31405bdb0529d6d3992407ef1ff0303c7072
Nirmalya Nayak <n.nayak263@gmail.com>
2023-02-22 10:44:37 +0530

    added copy to clipboard feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 // Import Assets
-import profile from '../assets/profile.jpg'
+import profile from "../assets/profile.jpg";
 
 const Header = () => {
   return (
@@ -10,10 +10,24 @@ const Header = () => {
         <h1>Hi, I'm Nirmalya Nayak</h1>
         <p>Full Stack / Blockchain Developer</p>
         <h4>
-          E-Mail:{' '}
+          E-Mail:{" "}
           <a href="mailto:nayak.nirmalya@hotmail.com">
             nayak.nirmalya@hotmail.com
           </a>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link>{" "}
+          <div class="tooltip">
+            <i
+              className="fa fa-copy"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                navigator.clipboard.writeText("nayak.nirmalya@hotmail.com");
+              }}
+            ></i>
+            <span class="tooltiptext">Copy to Clipboard.</span>
+          </div>
         </h4>
         <h4>
           Contact No.: <a href="tel:+917008944644">+91-7008944644</a>
@@ -21,7 +35,7 @@ const Header = () => {
         <h4>Odisha, India</h4>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
