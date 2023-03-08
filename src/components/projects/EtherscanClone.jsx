0abbd19c3309502d63716289d29f07261206aad1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-08 08:45:17 +0530

    added about of EtherscanClone

--- src/components/projects/EtherscanClone.jsx
+++ src/components/projects/EtherscanClone.jsx
@@ -5,7 +5,11 @@ const EtherscanClone = () => {
     <div className="projects__card">
       <h3>Etherscan Clone</h3>
       <img src={etherscan_clone} alt="Etherscan Clone" />
-      <p>It is a clone of popular Ethereum block explorer Etherscan.</p>
+      <p>
+        It is a clone of popular Ethereum block explorer Etherscan. Where anyone
+        can view details of latest blocks and transactions, also transactions of
+        a wallet address.
+      </p>
       <div className="centered">
         <a
           href="https://github.com/nayak-nirmalya/etherscan-clone"
