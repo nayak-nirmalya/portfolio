638218abb0d1e3fd8082a268e48c03885626a145
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-30 15:11:25 +0530

    change button order in rddit

--- src/components/projects/RedditFirebase.jsx
+++ src/components/projects/RedditFirebase.jsx
@@ -12,20 +12,20 @@ const RedditFirebase = () => {
       </p>
       <div className="centered">
         <a
-          href="https://github.com/nayak-nirmalya/reddit-clone-full-stack"
-          target="_blank"
-          rel="noreferrer"
-          className="button"
-        >
-          Code
-        </a>
-        <a
           href="https://reddit-clone-full-stack.vercel.app/"
           target="_blank"
           rel="noreferrer"
           className="button"
         >
           Live
+        </a>
+        <a
+          href="https://github.com/nayak-nirmalya/reddit-clone-full-stack"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Code
         </a>
       </div>
     </div>
