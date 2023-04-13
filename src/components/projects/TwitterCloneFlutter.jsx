bf19967c9f07be54f54fb7865cf02643b621acc8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-13 19:57:35 +0530

    added twitter clone flutter project card

---
+++ src/components/projects/TwitterCloneFlutter.jsx
@@ -0,0 +1,27 @@
+import twitter_flutter_flutter from "../../assets/twitter_flutter_flutter.png";
+
+const TwitterCloneFlutter = () => {
+  return (
+    <div className="projects__card">
+      <h3>Twitter Clone - Flutter</h3>
+      <img src={twitter_flutter_flutter} alt="TwitterCloneFlutter" />
+      <p>
+        Full Stack Twitter Clone Made using Dart, Flutter, AppWrite, Riverpod.
+        Funcionalities includes Notification, Following / Liking / Commenting /
+        Image Upload.
+      </p>
+      <div className="centered">
+        <a
+          href="https://github.com/nayak-nirmalya/twitter_clone_flutter"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Code
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default TwitterCloneFlutter;
