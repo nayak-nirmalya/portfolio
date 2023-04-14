cd8517c3ae561943bb696cb1339657f79970e336
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-14 19:08:18 +0530

    added react icons

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import { createContext, useState } from 'react'
-import ReactSwitch from 'react-switch'
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+import { BsSunFill, BsSun } from "react-icons/bs";
 
 // Import Components
-import Header from './components/Header'
-import About from './components/About'
-import Projects from './components/Projects'
-import Experience from './components/Experience'
-import Social from './components/Social'
-import Footer from './components/Footer'
-import Skills from './components/Slills'
-import Education from './components/Education'
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Social from "./components/Social";
+import Footer from "./components/Footer";
+import Skills from "./components/Slills";
+import Education from "./components/Education";
 
-export const ThemeContext = createContext(null)
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'))
-  }
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  };
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className="App" id={theme}>
         <div className="switch">
-          {/* <label>{theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label> */}
-          <ReactSwitch
-            onChange={toggleTheme}
-            checked={theme === 'dark'}
-            height={20}
-            width={50}
-          />
+          {theme === "light" ? (
+            <BsSun
+              size={34}
+              onClick={toggleTheme}
+              style={{ userSelect: "none", cursor: "pointer" }}
+            />
+          ) : (
+            <BsSunFill
+              size={34}
+              onClick={toggleTheme}
+              style={{ userSelect: "none", cursor: "pointer" }}
+            />
+          )}
         </div>
         <Header />
         <About />
@@ -42,7 +50,7 @@ function App() {
         <Footer />
       </div>
     </ThemeContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
