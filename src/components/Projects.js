e21b219034fe54a8a71d23c919fe40d24f3e28f8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-04-27 22:41:04 +0530

    added Deliveroo Clone

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,6 +24,7 @@ import ETHMarketplace from "./projects/ETHMarketplace";
 import EmployeeManagement from "./projects/EmployeeMGMT";
 import CrowdFundingDApp from "./projects/CrowdFundingDApp";
 import TwitterCloneFlutter from "./projects/TwitterCloneFlutter";
+import DeliverooClone from "./projects/DeliverooClone";
 
 const Projects = () => {
   const responsive = {
@@ -72,6 +73,7 @@ const Projects = () => {
         <TwitterCloneFlutter />
         <AmazonClone />
         <NodeShop />
+        <DeliverooClone />
         <PayPalClone />
         <MillowDApp />
         <GraphQLBlog />
---
+++ src/components/projects/DeliverooClone.jsx
@@ -0,0 +1,27 @@
+import deliveroo_clone from "../../assets/deliveroo_clone.png";
+
+const DeliverooClone = () => {
+  return (
+    <div className="projects__card">
+      <h3>Deliveroo Clone (Expo)</h3>
+      <img src={deliveroo_clone} alt="Deliveroo Clone" />
+      <p>
+        This Project is a Deliveroo Clone Mobile App with React Native, Expo,
+        JavaScript, TailWindCSS, Sanity CMS, Redux, React Native Navigation &
+        GROQ.
+      </p>
+      <div className="centered">
+        <a
+          href="https://github.com/nayak-nirmalya/deliveroo-clone"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Code
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default DeliverooClone;
