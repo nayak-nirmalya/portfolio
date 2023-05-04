369d0c5f1ea60224482233d36b54e2b4a5d67b00
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-04 13:44:50 +0530

    fixed list bullet point not showing

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,20 +1,20 @@
 const Education = () => {
   return (
     <div className="experience">
-      <h2 className="font-bold">Education</h2>
+      <h2>Education</h2>
 
-      <ul>
+      <ul className="list-disc">
         <li>
           <i>Veer Surendra Sai University of Technology</i> -{" "}
           <strong>MCA</strong>, 2018-2021
-          <ul>
+          <ul className="list-disc">
             <li>Master in Computer Application with 8.36/10 CGPA</li>
           </ul>
         </li>
         <li>
           <i>Kendrapara Autonomous College</i> - <strong>B.Sc (Physics)</strong>
           , 2015-2018
-          <ul>
+          <ul className="list-disc">
             <li>Graduated in Physics (Honours) with 8.21/10 CGPA</li>
           </ul>
         </li>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,10 +3,10 @@ const Experience = () => {
     <div className="experience">
       <h2 className="font-bold">Experience</h2>
 
-      <ul>
+      <ul className="list-disc">
         <li>
           <i>Cognizant</i> - <strong>Programmer Analyst</strong>
-          <ul>
+          <ul className="list-disc">
             <li>Worked in Java, Spring Framework</li>
             <li>Automated test cases with Selenium</li>
           </ul>
