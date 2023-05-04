a960305299f38a299d78d3ff9b429a69aa52f52f
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-04 12:07:01 +0530

    adding skills imgs

--- src/components/Slills.js
+++ src/components/Slills.js
@@ -4,83 +4,271 @@ const Skills = () => {
       <h2>Technical Skills</h2>
 
       <div className="column-list">
-        <ul>
-          <li>
-            <i>Node.js</i>
-          </li>
-          <li>
-            <i>Express</i>
-          </li>
-          <li>
-            <i>MongoDB</i>
-          </li>
-          <li>
-            <i>MySQL</i>
-          </li>
-          <li>
-            <i>React Native</i>
-          </li>
-          <li>
-            <i>Next.js</i>
-          </li>
-          <li>
-            <i>Appwrite</i>
-          </li>
-          <li>
-            <i>REST API</i>
-          </li>
-          <li>
-            <i>GraphQL</i>
-          </li>
-          <li>
-            <i>Solidity</i>
-          </li>
-          <li>
-            <i>TailwindCSS</i>
-          </li>
-          <li>
-            <i>Google Firebase</i>
-          </li>
-          <li>
-            <i>VS Code</i>
-          </li>
-          <li>
-            <i>Truffle</i>
-          </li>
-          <li>
-            <i>Hardhat</i>
-          </li>
-          <li>
-            <i>Web3.js</i>
-          </li>
-          <li>
-            <i>Ether.js</i>
-          </li>
-          <li>
-            <i>React</i>
-          </li>
-          <li>
-            <i>Mocha/Chai</i>
-          </li>
-          <li>
-            <i>Python</i>
-          </li>
-          <li>
-            <i>Java</i>
-          </li>
-          <li>
-            <i>AI/ML</i>
-          </li>
-          <li>
-            <i>AWS/GCP</i>
-          </li>
-          <li>
-            <i>Linux</i>
-          </li>
-        </ul>
+        <a href="#">
+          <img
+            alt="JavaScript"
+            src="https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Solidity"
+            src="https://img.shields.io/badge/Solidity-%23363636.svg?style=for-the-badge&logo=solidity&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="TypeScript"
+            src="https://img.shields.io/badge/TypeScript-007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Node.js"
+            src="https://img.shields.io/badge/Node.js-43853D.svg?style=for-the-badge&logo=node.js&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Python"
+            src="https://img.shields.io/badge/Python-14354C.svg?style=for-the-badge&logo=python&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Java"
+            src="https://custom-icon-badges.demolab.com/badge/Java-007396.svg?style=for-the-badge&logo=java&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="SQL"
+            src="https://custom-icon-badges.demolab.com/badge/SQL-025E8C.svg?style=for-the-badge&logo=database&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Dart"
+            src="https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="HTML"
+            src="https://img.shields.io/badge/HTML-E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="CSS"
+            src="https://img.shields.io/badge/CSS-1572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Markdown"
+            src="https://img.shields.io/badge/Markdown-000000.svg?style=for-the-badge&logo=markdown&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Bash"
+            src="https://img.shields.io/badge/Bash-121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white"
+          />
+        </a>
+
+        <a href="#">
+          <img
+            alt="Ethereum"
+            src="https://img.shields.io/badge/Ethereum-3C3C3D?style=for-the-badge&logo=Ethereum&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="React"
+            src="https://img.shields.io/badge/React-20232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Next"
+            src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="React Native"
+            src="https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Expo"
+            src="https://img.shields.io/badge/expo-1C1E24?style=for-the-badge&logo=expo&logoColor=#D04A37"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Flutter"
+            src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="tailwindcss"
+            src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="PNPM"
+            src="https://img.shields.io/badge/pnpm-%234a4a4a.svg?style=for-the-badge&logo=pnpm&logoColor=f69220"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Redux"
+            src="https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Vite"
+            src="https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Express.js"
+            src="https://img.shields.io/badge/Express.js-404d59.svg?style=for-the-badge&logo=express&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Flask"
+            src="https://img.shields.io/badge/Flask-000000.svg?style=for-the-badge&logo=flask&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="DJango"
+            src="https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Spring"
+            src="https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="scikit--learn"
+            src="https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="NumPy"
+            src="https://img.shields.io/badge/Numpy-013243.svg?style=for-the-badge&logo=numpy&logoColor=white"
+          />
+        </a>
+        <a href="#">
+          <img
+            alt="Pandas"
+            src="https://img.shields.io/badge/Pandas-150458.svg?style=for-the-badge&logo=pandas&logoColor=white"
+          />
+        </a>
       </div>
     </div>
   );
 };
 
 export default Skills;
+
+// const Skills = () => {
+//   return (
+//     <div className="experience">
+//       <h2>Technical Skills</h2>
+
+//       <div className="column-list">
+//         <ul>
+//           <li>
+//             <i>Node.js</i>
+//           </li>
+//           <li>
+//             <i>Express</i>
+//           </li>
+//           <li>
+//             <i>MongoDB</i>
+//           </li>
+//           <li>
+//             <i>MySQL</i>
+//           </li>
+//           <li>
+//             <i>React Native</i>
+//           </li>
+//           <li>
+//             <i>Next.js</i>
+//           </li>
+//           <li>
+//             <i>Appwrite</i>
+//           </li>
+//           <li>
+//             <i>REST API</i>
+//           </li>
+//           <li>
+//             <i>GraphQL</i>
+//           </li>
+//           <li>
+//             <i>Solidity</i>
+//           </li>
+//           <li>
+//             <i>TailwindCSS</i>
+//           </li>
+//           <li>
+//             <i>Google Firebase</i>
+//           </li>
+//           <li>
+//             <i>VS Code</i>
+//           </li>
+//           <li>
+//             <i>Truffle</i>
+//           </li>
+//           <li>
+//             <i>Hardhat</i>
+//           </li>
+//           <li>
+//             <i>Web3.js</i>
+//           </li>
+//           <li>
+//             <i>Ether.js</i>
+//           </li>
+//           <li>
+//             <i>React</i>
+//           </li>
+//           <li>
+//             <i>Mocha/Chai</i>
+//           </li>
+//           <li>
+//             <i>Python</i>
+//           </li>
+//           <li>
+//             <i>Java</i>
+//           </li>
+//           <li>
+//             <i>AI/ML</i>
+//           </li>
+//           <li>
+//             <i>AWS/GCP</i>
+//           </li>
+//           <li>
+//             <i>Linux</i>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Skills;
