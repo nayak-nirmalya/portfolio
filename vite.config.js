24716837567ab96d56fd503e7428cd696eb82c6c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-20 20:41:19 +0530

    updated vite.config for .jsx

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
+    esbuild: {
+      loader: "jsx",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+        },
+      },
+    },
     build: {
       outDir: "build",
     },
