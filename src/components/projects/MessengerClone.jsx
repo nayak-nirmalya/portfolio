bc70dffcea060bbf33760a487086670e322225c4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-20 21:27:46 +0530

    updated Messenger Clone project details

--- src/components/projects/MessengerClone.jsx
+++ src/components/projects/MessengerClone.jsx
@@ -3,11 +3,12 @@ import messenger_clone from "../../assets/messenger_clone.png";
 const MessengerClone = () => {
   return (
     <div className="projects__card">
-      <h3>Messenger Clone - NextJS</h3>
+      <h3>Real-Time Messenger</h3>
       <img src={messenger_clone} alt="MessengerClone" />
       <p>
-        Full Stack Responsive Real-Time Messenger Clone made with Next.js 13,
-        App Router, React, Tailwind, Prisma, MongoDB, NextAuth, Pusher.
+        Full Stack Responsive, Real-Time Messenger Clone made with Next.js 13,
+        App Router, React, Tailwind, React Hook Form, Headless UI, Prisma,
+        MongoDB, NextAuth, Pusher.
       </p>
       <div className="centered">
         <a
