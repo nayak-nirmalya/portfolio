3e801b25547083cd8143fa133990149476945103
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-21 13:21:41 +0530

    added social badges remove old btn and styles

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -5,39 +5,49 @@ const Social = () => {
     <div className="social">
       <h2 className="font-bold">Socials</h2>
 
-      <div className="centered">
-        <a href="mailto:n.nayak263@gmail.com" className="button">
-          E-Mail Me!
+      <div className="flex flex-row items-center justify-center sm:gap-2 lg:gap-8">
+        <a href="mailto:n.nayak263@gmail.com" target="_blank" rel="noopener noreferrer">
+          <img
+            className="items-center mx-auto lg:h-10 sm:h-auto"
+            src="https://img.shields.io/badge/-GMail-c14438?style=for-the-badge&logo=Gmail&logoColor=white"
+            alt="n.nayak263@gmail.com"
+          />
         </a>
-        <a href="tel:+917008944644" className="button">
-          Call Me!
+
+        <a href="https://wa.me/917008944644" target="_blank" rel="noopener noreferrer">
+          <img
+            className="items-center mx-auto lg:h-10 sm:h-auto"
+            src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"
+            alt="+91-7008944644"
+          />
         </a>
+
         <a
           href="https://www.linkedin.com/in/nirmalya-nayak/"
           target="_blank"
-          rel="noreferrer"
-          className="button"
+          rel="noopener noreferrer"
         >
-          LinkedIn
+          <img
+            className="items-center mx-auto lg:h-10 sm:h-auto"
+            src="https://img.shields.io/badge/-LinkedIn-blue?style=for-the-badge&logo=Linkedin&logoColor=white"
+            alt="nirmalya-nayak"
+          />
         </a>
-        <a
-          href="https://github.com/nayak-nirmalya"
-          target="_blank"
-          rel="noreferrer"
-          className="button"
-        >
-          GitHub
+
+        <a href="https://github.com/nayak-nirmalya" target="_blank" rel="noopener noreferrer">
+          <img
+            className="items-center mx-auto lg:h-10 sm:h-auto"
+            src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white"
+            alt="nayak-nirmalya"
+          />
         </a>
-        <a
-          href="https://www.hackerrank.com/n_nayak"
-          target="_blank"
-          rel="noreferrer"
-          className="button"
-        >
-          HackerRank
-        </a>
-        <a href="https://wa.me/917008944644" target="_blank" rel="noreferrer" className="button">
-          WhatsApp
+
+        <a href="https://www.hackerrank.com/n_nayak" target="_blank" rel="noopener noreferrer">
+          <img
+            className="items-center mx-auto lg:h-10 sm:h-auto"
+            src="https://img.shields.io/badge/-HackerRank-2EC866?style=for-the-badge&logo=HackerRank&logoColor=white"
+            alt="nayak-nirmalya"
+          />
         </a>
       </div>
     </div>
