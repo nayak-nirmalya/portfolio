5e0986388a5e24f5a56e442aacf6ee200f1a5fc0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-21 06:40:53 +0530

    installed packages for eslint

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
-    // esbuild: {
-    //   loader: "jsx",
-    // },
-    // optimizeDeps: {
-    //   esbuildOptions: {
-    //     loader: {
-    //       ".js": "jsx",
-    //     },
-    //   },
-    // },
     build: {
       outDir: "build",
     },
