c2f758caf1b5948b31156231f05b9ca32bb581b5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-21 06:38:13 +0530

    configure .prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export const ThemeContext = createContext(null);
 function App() {
   const [theme, setTheme] = useState("");
 
-  const setThemeInStorage = (theme) => {
+  const setThemeInStorage = (theme: string) => {
     localStorage.setItem("theme", theme);
   };
 
