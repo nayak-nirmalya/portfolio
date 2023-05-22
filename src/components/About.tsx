8a97c29ddb634eae24455351298bc350ebed799c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-22 17:27:43 +0530

    added tailwind styling to custom dot

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { ThemeContext } from "../App";
+import { ThemeContext } from "@/App";
 
 const About = () => {
   const theme = useContext(ThemeContext);
--- src/components/CustomDot.tsx
+++ src/components/CustomDot.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { ThemeContext } from "@/App";
+import React, { useContext } from "react";
 
 interface CustomDotProps {
   onClick?: any;
@@ -6,9 +7,19 @@ interface CustomDotProps {
 }
 
 const CustomDot: React.FC<CustomDotProps> = ({ onClick, active }) => {
+  const theme = useContext(ThemeContext);
+
   return (
     <li className="hidden cursor-pointer lg:inline-block" onClick={() => onClick()}>
-      {active ? <span className="slickdot active"></span> : <span className="slickdot"></span>}
+      {active ? (
+        <span className="m-1 inline-block h-2 w-2 rounded-full bg-[#2ea043] p-1"></span>
+      ) : (
+        <span
+          className={`m-1 inline-block h-2 w-2 rounded-full ${
+            theme === "light" ? "bg-slate-900" : "bg-slate-50"
+          } p-1`}
+        ></span>
+      )}
     </li>
   );
 };
