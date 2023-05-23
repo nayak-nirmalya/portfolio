ab4ea4270373203876f0edfd36da72f3b6053a35
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-23 10:32:32 +0530

    added title to ScrollToTop btn

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -22,7 +22,7 @@ const ScrollToTop = () => {
       id="topButton"
       className="fixed bottom-5 right-5 z-10 hidden animate-bounce rounded-full bg-[#008800] p-2 shadow-md hover:bg-[#2b611a] lg:bottom-10 lg:right-10"
     >
-      <BiArrowToTop color="white" size={25} />
+      <BiArrowToTop title="Scroll to Top." color="white" size={25} />
     </button>
   );
 };
