7454eaaa8c5b0679480d3f520ec9498c0e809b55
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-23 08:51:36 +0530

    added custom animation

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,6 +1,6 @@
 import { ThemeContextType } from "@/App";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BsSunFill, BsSun } from "react-icons/bs";
 
 interface ToggleThemeProps {
@@ -9,6 +9,8 @@ interface ToggleThemeProps {
 }
 
 const ToggleTheme: React.FC<ToggleThemeProps> = ({ theme, setTheme }) => {
+  const [effect, setEffect] = useState(false);
+
   const setThemeInStorage = (theme: string) => {
     localStorage.setItem("theme", theme);
   };
@@ -39,12 +41,26 @@ const ToggleTheme: React.FC<ToggleThemeProps> = ({ theme, setTheme }) => {
   return (
     <div className="mx-8 flex justify-end p-4 lg:mx-52">
       {theme === "light" ? (
-        <BsSun size={34} onClick={toggleTheme} style={{ userSelect: "none", cursor: "pointer" }} />
+        <BsSun
+          className={`${effect && "animate-rotateSwitch"} cursor-pointer select-none`}
+          size={34}
+          onClick={() => {
+            toggleTheme();
+            setEffect(true);
+          }}
+          onAnimationEnd={() => setEffect(false)}
+        />
       ) : (
         <BsSunFill
+          className={`${
+            effect && "animate-rotateSwitch"
+          } cursor-pointer select-none transition-all hover:text-gray-400`}
           size={34}
-          onClick={toggleTheme}
-          style={{ userSelect: "none", cursor: "pointer" }}
+          onClick={() => {
+            toggleTheme();
+            setEffect(true);
+          }}
+          onAnimationEnd={() => setEffect(false)}
         />
       )}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotateSwitch: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(45deg)" }
+        }
+      },
+      animation: {
+        rotateSwitch: "rotateSwitch 250ms ease-in-out"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
