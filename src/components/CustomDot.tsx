54d2b47851ce7a1fcf2983b5ca9ac49f4411ec60
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-23 11:07:35 +0530

    show dot on sm screen sizes

--- src/components/CustomDot.tsx
+++ src/components/CustomDot.tsx
@@ -11,14 +11,14 @@ const CustomDot: React.FC<CustomDotProps> = ({ onClick, active }) => {
   const theme = useContext(ThemeContext);
 
   return (
-    <li className="hidden cursor-pointer lg:inline-block" onClick={() => onClick()}>
+    <li className="cursor-pointer lg:inline-block" onClick={() => onClick()}>
       {active ? (
-        <span className="m-1 inline-block h-2 w-2 rounded-full bg-[#2ea043] p-1"></span>
+        <span className="m-0.5 inline-block h-0.5 w-0.5 rounded-full bg-[#2ea043] p-0.5 lg:m-1 lg:h-2 lg:w-2 lg:p-1"></span>
       ) : (
         <span
-          className={`m-1 inline-block h-2 w-2 rounded-full ${
+          className={`m-0.5 inline-block h-0.5 w-0.5 rounded-full lg:m-1 lg:h-2 lg:w-2 ${
             theme === "light" ? "bg-slate-900" : "bg-slate-50"
-          } p-1`}
+          } p-0.5 lg:p-1`}
         ></span>
       )}
     </li>
