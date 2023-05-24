961f534044ddd6225f5931f9261cbe4e070b91cf
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-24 22:11:12 +0530

    added transition to progressbar

--- src/components/ReadProgressBar.tsx
+++ src/components/ReadProgressBar.tsx
@@ -9,7 +9,7 @@ const ReadProgressBar = () => {
     <nav className="sticky top-0 z-50 py-0.5">
       <span
         style={{ transform: `translateX(${completion - 100}%)` }}
-        className="absolute bottom-0 h-1 w-full bg-[#008800]"
+        className="absolute bottom-0 h-1 w-full bg-[#008800] transition"
       />
     </nav>
   );
