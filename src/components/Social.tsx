57546a1c0e40f92a9861f266e698fee2e6d32587
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-24 14:34:56 +0530

    updated discord link

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -67,20 +67,20 @@ const Social = () => {
           <img
             className="mx-auto h-8 items-center transition-all duration-200 hover:scale-110"
             src="https://img.shields.io/badge/-HackerRank-2EC866?style=for-the-badge&logo=HackerRank&logoColor=white"
-            alt="nayak-nirmalya"
+            alt="n_nayak"
           />
         </a>
 
         <a
           className="col-start-3 col-end-7"
-          href="https://www.linkedin.com/in/nirmalya-nayak/"
+          href="http://discordapp.com/users/536800905727705118"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img
             className="mx-auto h-8 items-center transition-all duration-200 hover:scale-110"
             src="https://img.shields.io/badge/Discord-%235865F2.svg?style=for-the-badge&logo=discord&logoColor=white"
-            alt="nirmalya-nayak"
+            alt="nirmalya#1607"
           />
         </a>
 
