7976d0e023f597ee957e2c9980b6f4ff72de7ed0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-24 12:28:27 +0530

    updated our lovely footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mx-auto justify-center pb-2 text-center lg:pb-6">
+    <footer className="mx-auto justify-center pb-4 text-center lg:pb-6">
       <small className="text-xs">
         Made with ❤️ by Nirmalya in
         <img
-          className="mx-1.5 inline-block h-4 w-4 align-text-bottom"
+          className="mx-1.5 inline-block h-4 w-4 lg:align-text-bottom"
           src="/india-flag.svg"
           alt="India Flag"
         />
