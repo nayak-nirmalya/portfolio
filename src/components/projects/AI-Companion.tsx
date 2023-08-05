61f12f9dc984c62f1c2c15af46d618301d7a47f0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-05 14:02:33 +0530

    added AI_Companion to projects list

---
+++ src/components/projects/AI-Companion.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import ai_companion from "../../assets/ai_companion.png";
+
+const AI_Companion = () => {
+  return (
+    <div className="projects__card">
+      <h3>AI Companion</h3>
+      <img src={ai_companion} alt="AI_Companion" />
+      <p>Create personalized AI charecters your companion & chat with them, ask questions.</p>
+      <div className="centered">
+        <a
+          href="https://ai-companion-beige.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Demo
+        </a>
+        <a
+          href="https://github.com/nayak-nirmalya/ai-companion"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Code
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AI_Companion;
