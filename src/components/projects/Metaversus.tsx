6f9da2d6329471b7e01cef9efdba9752ae7d6fb9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-08-26 09:00:48 +0530

    added Metaversus to projects list

---
+++ src/components/projects/Metaversus.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import metaversus from "@assets/metaversus.png";
+
+const Metaversus = () => {
+  return (
+    <div className="projects__card">
+      <h3>AI Companion</h3>
+      <img src={metaversus} alt="Metaversus" />
+      <p>
+        A beautiful, fully responsive, animated dummy Landing Page. Made using Next.js, React,
+        Framer Motion & Tailwind.
+      </p>
+      <div className="centered">
+        <a
+          href="https://metaverse-landing-smoky.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Demo
+        </a>
+        <a
+          href="https://github.com/nayak-nirmalya/metaverse-landing"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Code
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Metaversus;
