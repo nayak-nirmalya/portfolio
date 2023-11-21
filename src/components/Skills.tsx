f231c237091eab5582e1c9c59ff2c62fe373fae5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-11-21 09:25:28 +0530

    added docker to skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -243,6 +243,13 @@ const Skills = () => {
 
         <img
           className="mx-auto h-8 items-center transition-all hover:scale-110"
+          alt="Docker"
+          title="Docker"
+          src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white"
+        />
+
+        <img
+          className="mx-auto h-8 items-center transition-all hover:scale-110"
           alt="Google Firebase"
           title="Google Firebase"
           src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase"
