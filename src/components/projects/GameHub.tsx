ddbe84dbe788c6d9e42cfd0161de16685dd6f7f3
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-16 19:22:50 +0530

    createed GameHub component

---
+++ src/components/projects/GameHub.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import twitch_clone from "@assets/twitch_clone.png";
+
+const GameHub = () => {
+  return (
+    <div className="projects__card">
+      <h3>GameHub - Twitch Clone</h3>
+      <img src={twitch_clone} alt="AI_SaaS" />
+      <p>
+        FullStack Twitch Clone with Next.js 14, Livestreaming, React, Prisma, Stripe, Tailwind,
+        shadcn/ui, MySQL & TypeScript. Go live using RTMP / WHIP protocols with OBS & chat with
+        viewers.
+      </p>
+      <div className="centered">
+        <a
+          href="https://twitch-clone-orcin.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Demo
+        </a>
+        <a
+          href="https://github.com/nayak-nirmalya/twitch-clone"
+          target="_blank"
+          rel="noreferrer"
+          className="button"
+        >
+          Code
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default GameHub;
